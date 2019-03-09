059fd4faf141ea3c5764d5d927d6c8d346a348bf
Ruthger Idema <ruthger.idema@gmail.com>
2019-03-09 15:07:13 +0000

    Apply fixes from StyleCI

--- app/Product.php
+++ app/Product.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Http\Controllers\ProductController;
 use RuthgerIdema\UrlRewrite\Facades\UrlRewrite;
 use RuthgerIdema\UrlRewrite\Traits\HasUrlRewrite;
 
