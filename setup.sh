40a2717148c9269095840e5efc05f091dc7dbe20
Ruthger Idema <ruthger.idema@guapa.nl>
2019-03-12 11:53:26 +0100

    Updated to 1.1.0

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 docker-compose up -d
 docker-compose exec app composer install
-docker-compose exec app php artisan migrate
+docker-compose exec app php artisan migrate --force
+docker-compose exec app php artisan migrate:refresh --force
 docker-compose exec app php artisan db:seed --class=CatalogSeeder
 
